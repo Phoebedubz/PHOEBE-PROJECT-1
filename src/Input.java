19fc18fc69f8daeed0f47a4972801566bc47b439
100144016 <100144016@lubbockisd.net>
2023-09-14 16:18:53 -0500

    WHh     qryfgiagf

---
+++ src/Input.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//NAME - Phoebe DuBose
+//DATE - 09/14/23
+//PURPOSE - Computer Science A
+
+import java.util.Scanner;
+
+public class Input
+
+{
+public static void main (String[] args)
+{
+Scanner keyboard = new Scanner(System.in);
+int intOne, intTwo;
+boolean bolOne = true;
+double douOne = 0;
+System.out.print("Enter integer one :: ");
+intOne = keyboard.nextInt();
+System.out.print("Enter integer two :: ");
+intTwo = keyboard.nextInt();
+System.out.print("Enter boolean one :: ");
+System.out.print("Enter double one :: ");
+System.out.println("integer one = " + intOne);
+System.out.println("integer two = " + intTwo);
+System.out.println("boolean one = " + bolOne);
+System.out.println("double one = " + douOne);
+
+
+//add more code here for the other variables and data types
+}
+}
